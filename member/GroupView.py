5728911edccdc89a2a15d6e64900d04ee8fa5b88
pooja2012 <33168116+pooja2012@users.noreply.github.com>
2018-03-02 19:18:02 +0000

    Update GroupView.py
    added loggers

--- member/GroupView.py
+++ member/GroupView.py
@@ -5,7 +5,13 @@ The group view is updated with no more than one change at a time - either an add
 Using a list ensures ordering whilst also allowing for arbitrary removal of nodes at any position.
 Checks are in place to prevent duplicates from being added.
 '''
+import logging
 
+logging.basicConfig(
+    filename="DistributedManagementSystem.log",
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(message)s"
+    )
 
 class GroupView:
 
@@ -14,22 +20,32 @@ class GroupView:
 
     # Add a member to the group view
     def add_member(self, new_member):
+        
         if not self.members.__contains__(new_member):
+            
             self.members.append(new_member)
             print('Added new member {0} to the group view'.format(new_member.name))
+            logging.info('Added new member {0} to the group view'.format(new_member.name))
+            
         else:
-            # TODO Pooja this may need to be added to the log
+           
             print('Member {0} was already in the group view - did not add duplicate.'.format(new_member.name))
-
+            logging.info('Member {0} was already in the group view - did not add duplicate.'.format(new_member.name))
+            
     # Remove a member from the group view
     def remove_member(self, old_member):
+        
         if self.members.contains(old_member):
+            
             self.members.remove(old_member)
             print('Removed member {0} from the group view'.format(old_member.name))
+            logging.info('Removed member {0} from the group view'.format(old_member.name))
+            
         else:
-            # TODO Pooja this may need to be added to the log
+        
             print('Member {0} was not in the group view - did not attempt removal.'.format(old_member.name))
-
+            logging.info('Member {0} was not in the group view - did not attempt removal.'.format(old_member.name))
+            
     def get_group_view(self):
         return self.members
 
