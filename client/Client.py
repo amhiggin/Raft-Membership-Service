8ec73b9eaaf2249cd63d9dd72e1a826da045d204
amhiggin <amhiggin@tcd.ie>
2018-03-10 14:47:10 +0000

    Adding user input options to the client. Updated the ClientLib to use timestamps in console output. Added a launch script for the client. TODO: implement sending of request to the leader and receiving and outputting the response.

--- client/Client.py
+++ client/Client.py
@@ -1,18 +1,35 @@
 '''
-This is a client which will connect to the distributed system for test purposes.
-To be decided: communication protocol, etc.
+    This is a client which will connect to the distributed system for test purposes.
+    To be decided: communication protocol, etc.
 '''
-
+import sys
+sys.path.append("../")
 import ClientLib as lib
 
-
-def print_message(message):
-    print('Client:\t' + message + '\n')
+DISPLAY_USER_OPTIONS = "---------\n>> {0} Client:\t".format(lib.get_timestamp()) + \
+                       "Enter:\n\t- 1 to request the current group membership.\n\t- x to terminate."
 
 
 def main():
-    print('Hello world from client')
-    lib.send_request()
+    lib.print_message('Hello world from client')
+    running = True
+
+    while running:
+        # Display user options until they decide to exit
+        try:
+            from pip._vendor.distlib.compat import raw_input
+            user_input = raw_input(DISPLAY_USER_OPTIONS)
+            if user_input == "1":
+                response = lib.send_request()
+            elif user_input == 'x':
+                running = False
+            else:
+                lib.print_message("You said: " + user_input + ", which is invalid. Give it another go!\n")
+        except Exception as e:
+            lib.print_message('An error occurred during client operation: ' + e.message)
+
+    lib.print_message("Terminating... Goodbye world!")
+    exit(0)
 
 
 if __name__ == "__main__":
--- client/ClientLib.py
+++ client/ClientLib.py
@@ -1,13 +1,22 @@
 import socket
+import time, datetime
+import Message as Message
+import MessageType as MessageType
 
 
 def send_request():
     '''
-    TODO implement
     The client will need to be able to know where to send the request to
     '''
-
-    # Note that there is a MessageType.service enum entry for a client service request: though this should
-    # be used by the servers
+    print_message('Sending a request for the members of the group')
+    message = Message.Message(None, MessageType.MessageType.service, '')
+    # Todo send this
     pass
 
+
+def get_timestamp():
+    return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def print_message(message):
+    print('>> {0} Client:\t'.format(get_timestamp()) + message)
