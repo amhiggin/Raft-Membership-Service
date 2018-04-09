4e21b5c895b1673ca259ee5e00baf699fffaf749
amhiggin <amhiggin@tcd.ie>
2018-04-09 19:04:26 +0100

    Adding a note in the launch_client script that the option '3' needs to be selected before option '1' or '2'

--- launch_client.sh
+++ launch_client.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#
+# Note: for options 1 and 2, the client requires that the address of the group
+#       first be obtained by selecting option '3'.
+#
+
+
 PORT=${1:-8900};
 
 # Launch a single client
