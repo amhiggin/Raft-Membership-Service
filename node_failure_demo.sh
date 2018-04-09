8d6c7a448256e3207f47d12c5abbf2f5c69d4aa8
Apple <apple@Apples-MacBook-Pro-6.local>
2018-04-09 18:22:24 +0100

    node failure final push

--- node_failure_demo.sh
+++ node_failure_demo.sh
@@ -3,22 +3,12 @@
 # Create directory for writing logs to, if it does not already exist
 mkdir -p MemberLogs;
 
-#echo "Launching $NUM_SERVERS nodes"
-
-#WAIT_TIME = ${1:-10};
-#SLEEP_TIME = ${2:-10};
-
-#for i in $( seq 2 5 )
-#do
 python3 "$(pwd)"/member/Member.py False 0 0 0 &
-#done
+python3 "$(pwd)"/member/Member.py False 0 0 0 &
 
-#for i in $( seq 6 8)
-#do
-python3 "$(pwd)"/member/Member.py False 0 10 10 &
-echo "Node will fail for 10 seconds after waiting 10 seconds" &
-#done
-# python3 "$(pwd)"/member/Member.py True 0
+python3 "$(pwd)"/member/Member.py False 0 20 20 &
+echo "Node will fail for 20 seconds after waiting 20 seconds" &
+
 python3 member/Member.py True 0
 
 
